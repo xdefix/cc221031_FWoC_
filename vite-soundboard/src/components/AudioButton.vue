a2b0cdd310525ec42e7e0db9761146e15464710d
Roman Kuba <roman.kuba@gmail.com>
2023-04-20 11:58:50 +0200

    Vite Soundboard Working with Components

---
+++ vite-soundboard/src/components/AudioButton.vue
@@ -0,0 +1,27 @@
+<template>
+    <audio
+        :src="`/sounds/${sound}.wav`"
+        controls
+        ref="audioTag"
+        @ended="ended"></audio>
+    <button @click="playSound" :class="{ playing: isPlaying }">
+        {{ sound }}
+    </button>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const audioTag = ref();
+const isPlaying = ref(false);
+const props = defineProps(['sound']);
+
+const playSound = () => {
+    isPlaying.value = true;
+    audioTag.value.play();
+};
+
+const ended = () => {
+    isPlaying.value = false;
+};
+</script>
